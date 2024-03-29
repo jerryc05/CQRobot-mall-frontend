10e10a0a96c83a6e2fc41ec12c4d5feb788eb2a7
Jerry Chen <jerryc443@gmail.com>
2024-03-28 23:26:16 -0400

    refresh on 401

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,5 @@
-import axios from 'axios'
-import { setToken, type token } from '@/utils'
+import axios, { AxiosError } from 'axios'
+import { setToken, token } from '@/utils'
 
 export const users_register = (body: {
   email: string
@@ -17,15 +17,21 @@ export const users_login = (body: { email: string; password: string }) =>
       return x.data
     })
 
-export const users_me = () =>
-  axios
-    .get<{
-      email: string
-      first_name: string
-      last_name: string
-      phone_number: string
-    }>('/api/users/me')
-    .then(x => x.data)
+export async function users_me(canRefresh = true) {
+  try {
+    return await axios
+      .get<{
+        email: string
+        first_name: string
+        last_name: string
+        phone_number: string
+      }>('/api/users/me')
+      .then(x => x.data)
+  } catch (err) {
+    if (canRefresh) await refreshOnErr(err)
+    return await users_me(false)
+  }
+}
 
 export const users_logout = (refresh_token: string) =>
   axios.post('/api/users/logout').then(() => {
@@ -46,4 +52,10 @@ export const users_refresh_token = (refresh_token: string) =>
 function handleToken(tokens: Awaited<ReturnType<typeof users_login>>) {
   axios.defaults.headers.common.Authorization = `${tokens.token_type} ${tokens.access_token}`
   setToken(tokens)
+}
+
+async function refreshOnErr(err: any) {
+  if (err instanceof AxiosError && err.response?.status === 401) {
+    await users_refresh_token(token()?.refresh_token ?? '')
+  }
 }
