5fd029f779784bfa80619681f52d26514478e246
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2024-06-08 16:38:54 +0800

    carousel

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,12 @@
 import { home_carousel } from '@/api'
+import { Carousel } from 'solid-bootstrap'
 import { For, Index, createEffect, createSignal } from 'solid-js'
-import { Slider } from 'solid-slider'
-import { autoplay } from 'solid-slider/dist/autoplay/autoplay'
 import { BodyRightContent } from './HomeRightContent'
 
 export default function Home() {
   return (
     <div class='mx-20 my-2'>
-      <Carousel />
+      <CarouselComp />
       <div class='flex'>
         <BodyLeftPanel />
         <BodyRightContent />
@@ -16,23 +15,36 @@ export default function Home() {
   )
 }
 
-function Carousel() {
+function CarouselComp() {
   const [imgs, setImgs] = createSignal<string[]>()
   createEffect(() => {
     home_carousel().then(setImgs)
   })
+
+  const [index, setIndex] = createSignal(0)
+  const handleSelect = (
+    selectedIndex: number,
+    e: Record<string, unknown> | null
+  ) => {
+    setIndex(selectedIndex)
+  }
+
   return (
-    <Slider options={{ loop: true }} plugins={[autoplay(1500, {})]}>
+    <Carousel activeIndex={index()} onSelect={handleSelect}>
       <For each={imgs()}>
         {(x, i) => (
-          <img
-            class={`w-full h-56 my-1 bg-gray-200 ${i() === 0 ? '' : 'hidden'}`}
-            src={x}
-            alt={`carousel-${i()}`}
-          />
+          <Carousel.Item>
+            <img
+              class={`w-full h-56 my-1 bg-gray-200 ${
+                i() === 0 ? '' : 'hidden'
+              }`}
+              src={x}
+              alt={`carousel-${i()}`}
+            />
+          </Carousel.Item>
         )}
       </For>
-    </Slider>
+    </Carousel>
   )
 }
 
